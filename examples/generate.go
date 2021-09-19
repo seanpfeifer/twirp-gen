865a87e9c1f01131e86ae20db6569750ae467d2b
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2021-09-18 19:48:03 -0700

    Moved generated samples dir

--- examples/generate.go
+++ examples/generate.go
@@ -1,7 +1,4 @@
 package examples
 
-//go:generate protoc -I . --twirpjs_out=pathPrefix=/rpc:../pbgen/ ./*.proto
-//go:generate protoc -I . --twirpcs_out=pathPrefix=/rpc:../pbgen/ ./*.proto
-
-// NOTE: If you're doing both JS + C#, you can simplify to one line:
-//   protoc -I . --twirpjs_out=pathPrefix=/rpc:../pbgen/ --twirpjs_out=pathPrefix=/rpc:../pbgen/ ./*.proto
+//go:generate protoc -I . --twirpjs_out=pathPrefix=/rpc:../examples_gen/js ./*.proto
+//go:generate protoc -I . --twirpcs_out=pathPrefix=/rpc:../examples_gen/cs --csharp_out=../examples_gen/cs ./*.proto
