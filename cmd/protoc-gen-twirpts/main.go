9883a2b1897f1a34221e713ccb51fb9de3ecdb3c
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2022-10-19 18:24:12 -0700

    Initial pass at TypeScript support

---
+++ cmd/protoc-gen-twirpts/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	_ "embed"
+	"flag"
+	"strings"
+	"text/template"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	pluginpb "google.golang.org/protobuf/types/pluginpb"
+)
+
+const (
+	outFileName = "generated.ts"
+)
+
+//go:embed template.tmpl
+var fileTemplate string
+
+func main() {
+	// Set up our flags. The only one we care about for now is the server path prefix.
+	var flags flag.FlagSet
+	prefix := flags.String("pathPrefix", "/twirp", "the server path prefix to use, if modified from the Twirp default")
+
+	// No special options for this generator
+	opts := protogen.Options{ParamFunc: flags.Set}
+	opts.Run(func(plugin *protogen.Plugin) error {
+		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		out := plugin.NewGeneratedFile(outFileName, "")
+
+		in := jsData{
+			Files:      plugin.Files,
+			PathPrefix: *prefix,
+		}
+
+		template, err := template.New("file").
+			Funcs(template.FuncMap{
+				"JSName":  JSName,
+				"GetType": in.GetType,
+			}).
+			Parse(fileTemplate)
+		if err != nil {
+			return err
+		}
+
+		return template.Execute(out, in)
+	})
+}
+
+type jsData struct {
+	Files      []*protogen.File
+	PathPrefix string
+}
+
+func (j *jsData) GetType(desc protoreflect.FieldDescriptor) string {
+	switch desc.Kind() {
+	case protoreflect.BoolKind:
+		return "boolean"
+	case protoreflect.EnumKind:
+		return "number"
+	case protoreflect.Int32Kind:
+		return "number"
+	case protoreflect.Sint32Kind:
+		return "number"
+	case protoreflect.Uint32Kind:
+		return "number"
+	case protoreflect.Sfixed32Kind:
+		return "number"
+	case protoreflect.Fixed32Kind:
+		return "number"
+	case protoreflect.FloatKind:
+		return "number"
+	case protoreflect.Int64Kind:
+		return "bigint"
+	case protoreflect.Sint64Kind:
+		return "bigint"
+	case protoreflect.Uint64Kind:
+		return "bigint"
+	case protoreflect.Sfixed64Kind:
+		return "bigint"
+	case protoreflect.Fixed64Kind:
+		return "bigint"
+	case protoreflect.DoubleKind:
+		return "number"
+	case protoreflect.StringKind:
+		return "string"
+	case protoreflect.BytesKind:
+		// NOT using Uint8Array here, as these end up getting encoded/decoded as base64 strings
+		return "string"
+	case protoreflect.MessageKind:
+		// TODO: Check for types we've generated. For now, this will be "any"
+		return "any"
+	case protoreflect.GroupKind: // Not supported - explicitly a deprecated Protobuf feature
+		return "any"
+	default:
+		return "any"
+	}
+}
+
+// JSName exists as a way to get our camelCase method name.
+func JSName(m *protogen.Method) string {
+	if m.GoName == "" {
+		return ""
+	}
+	return strings.ToLower(m.GoName[:1]) + m.GoName[1:]
+}
--- examples/generate.go
+++ examples/generate.go
@@ -1,4 +1,5 @@
 package examples
 
 //go:generate protoc -I . --twirpjs_out=pathPrefix=/rpc:../examples_gen/js ./*.proto
+//go:generate protoc -I . --twirpts_out=pathPrefix=/rpc:../examples_gen/ts ./*.proto
 //go:generate protoc -I . --twirpcs_out=pathPrefix=/rpc:../examples_gen/cs --csharp_out=../examples_gen/cs ./*.proto
