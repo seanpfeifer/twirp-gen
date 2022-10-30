3cd3f47dd8d52012a513a0e830abc1692a4a6efb
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2022-10-29 18:06:02 -0700

    Add handling of map<> and enum types

--- cmd/protoc-gen-twirpts/main.go
+++ cmd/protoc-gen-twirpts/main.go
@@ -64,59 +64,51 @@ type jsData struct {
 }
 
 func (j *jsData) GetType(desc protoreflect.FieldDescriptor) string {
-	switch desc.Kind() {
-	case protoreflect.BoolKind:
-		return "boolean"
-	case protoreflect.EnumKind:
-		return "number"
-	case protoreflect.Int32Kind:
-		return "number"
-	case protoreflect.Sint32Kind:
-		return "number"
-	case protoreflect.Uint32Kind:
-		return "number"
-	case protoreflect.Sfixed32Kind:
-		return "number"
-	case protoreflect.Fixed32Kind:
-		return "number"
-	case protoreflect.FloatKind:
-		return "number"
-	case protoreflect.Int64Kind:
-		return "bigint"
-	case protoreflect.Sint64Kind:
-		return "bigint"
-	case protoreflect.Uint64Kind:
-		return "bigint"
-	case protoreflect.Sfixed64Kind:
-		return "bigint"
-	case protoreflect.Fixed64Kind:
-		return "bigint"
-	case protoreflect.DoubleKind:
-		return "number"
-	case protoreflect.StringKind:
-		return "string"
-	case protoreflect.BytesKind:
-		// NOT using Uint8Array here, as these end up getting encoded/decoded as base64 strings
-		return "string"
-	case protoreflect.MessageKind:
-		buf := &bytes.Buffer{}
-		buf.WriteString("{ ")
-		fields := desc.Message().Fields()
-		for i := 0; i < fields.Len(); i++ {
-			field := fields.Get(i)
-			if i > 0 {
-				buf.WriteString(", ")
-			}
-			buf.WriteString(field.JSONName())
-			buf.WriteString("?: ")
-			buf.WriteString(j.GetType(field))
-		}
-		buf.WriteString(" }")
-		return buf.String()
-	case protoreflect.GroupKind: // Not supported - explicitly a deprecated Protobuf feature
-		return "any"
+	switch {
+	case desc.IsMap():
+		return j.generateMap(desc)
 	default:
-		return "any"
+		switch desc.Kind() {
+		case protoreflect.BoolKind:
+			return "boolean"
+		case protoreflect.EnumKind:
+			return j.generateEnum(desc.Enum())
+		case protoreflect.Int32Kind:
+			return "number"
+		case protoreflect.Sint32Kind:
+			return "number"
+		case protoreflect.Uint32Kind:
+			return "number"
+		case protoreflect.Sfixed32Kind:
+			return "number"
+		case protoreflect.Fixed32Kind:
+			return "number"
+		case protoreflect.FloatKind:
+			return "number"
+		case protoreflect.Int64Kind:
+			return "bigint"
+		case protoreflect.Sint64Kind:
+			return "bigint"
+		case protoreflect.Uint64Kind:
+			return "bigint"
+		case protoreflect.Sfixed64Kind:
+			return "bigint"
+		case protoreflect.Fixed64Kind:
+			return "bigint"
+		case protoreflect.DoubleKind:
+			return "number"
+		case protoreflect.StringKind:
+			return "string"
+		case protoreflect.BytesKind:
+			// NOT using Uint8Array here, as these end up getting encoded/decoded as base64 strings
+			return "string"
+		case protoreflect.MessageKind:
+			return j.generateMessage(desc.Message())
+		case protoreflect.GroupKind: // Not supported - explicitly a deprecated Protobuf feature
+			return "any"
+		default:
+			return "any"
+		}
 	}
 }
 
@@ -126,4 +118,51 @@ func JSName(m *protogen.Method) string {
 		return ""
 	}
 	return strings.ToLower(m.GoName[:1]) + m.GoName[1:]
+}
+
+func (j *jsData) generateMessage(msg protoreflect.MessageDescriptor) string {
+	buf := bytes.Buffer{}
+	buf.WriteString("{ ")
+	fields := msg.Fields()
+	for i := 0; i < fields.Len(); i++ {
+		field := fields.Get(i)
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		buf.WriteString(field.JSONName())
+		buf.WriteString("?: ")
+		buf.WriteString(j.GetType(field))
+	}
+	buf.WriteString(" }")
+	return buf.String()
+}
+
+func (j *jsData) generateEnum(enum protoreflect.EnumDescriptor) string {
+	// Enums can be represented as either numbers or strings, but strings are easier to read
+	buf := bytes.Buffer{}
+	for i := 0; i < enum.Values().Len(); i++ {
+		if i != 0 {
+			buf.WriteString(` | `)
+		}
+		buf.WriteString(`"`)
+		buf.WriteString(string(enum.Values().Get(i).Name()))
+		buf.WriteString(`"`)
+	}
+	return buf.String()
+}
+
+func (j *jsData) generateMap(desc protoreflect.FieldDescriptor) string {
+	// Now we need to parse the map type - the key is always one of the primitive types,
+	// but the value can be a message or a primitive type.
+	buf := bytes.Buffer{}
+	switch j.GetType(desc.MapKey()) {
+	case "number":
+		buf.WriteString("NumberMap<")
+	default:
+		buf.WriteString("StringMap<")
+	}
+	buf.WriteString(j.GetType(desc.MapValue()))
+	buf.WriteString(">")
+
+	return buf.String()
 }
