234b773d2281e3f7bc022ca1b237f4d10a15f8c5
Sean Pfeifer <pfeifer.sean.a@gmail.com>
2022-11-13 02:32:55 -0800

    Add TypeScript generator (#5)
    * Fix comment on example
    
    * Initial pass at TypeScript support
    
    * Generate code for TypeScript
    
    * Factor out field listing to its own definition in another template
    
    * Add support for generating message types
    
    * Add custom parser for bigint
    
    * Add handling of map<> and enum types
    
    * Add example for new TS types
    
    * Separate msg + enum interface generation
    
    * Change request/response to generated interfaces
    
    * Add Class to TS request to ensure enums work as expected
    
    * Generate TS example code
    
    * Fix list generation, add Timestamp handling
    
    * Add handling for Struct known type
    
    * Add mappings for more known types
    
    * Add option for "proto case": non-camelcase fields
    
    * Add TS + option info to the README

--- README.md
+++ README.md
@@ -7,11 +7,14 @@ This exists because other code generators either required external dependencies 
 ## Installing
 
 ```sh
-# For JavaScript generation
-go install github.com/seanpfeifer/twirp-gen/cmd/protoc-gen-twirpjs@latest
+# For TypeScript generation
+go install github.com/seanpfeifer/twirp-gen/cmd/protoc-gen-twirpts@latest
 
 # For C# generation
 go install github.com/seanpfeifer/twirp-gen/cmd/protoc-gen-twirpcs@latest
+
+# For JavaScript generation
+go install github.com/seanpfeifer/twirp-gen/cmd/protoc-gen-twirpjs@latest
 ```
 
 Ensure your `~/go/bin` (Linux) or `%USERPROFILE%/go/bin` (Windows) are on your PATH.
@@ -20,6 +23,9 @@ Ensure your `~/go/bin` (Linux) or `%USERPROFILE%/go/bin` (Windows) are on your P
 
 ```sh
 # Typical Twirp usage to generate methods from the "examples" dir into "./examples_gen/generated.js"
+# For TypeScript
+protoc -I ./examples --twirpts_out=./examples_gen/ ./examples/*.proto
+# For JavaScript
 protoc -I ./examples --twirpjs_out=./examples_gen/ ./examples/*.proto
 
 # If you use twirp.WithServerPathPrefix(), eg `twirp.WithServerPathPrefix("/rpc")`, you can specify the
@@ -29,22 +35,42 @@ protoc -I ./examples --twirpjs_out=pathPrefix=/rpc:./examples_gen/ ./examples/*.
 # C# is the same as above two examples, but uses `--twirpcs_out`
 protoc -I ./examples --twirpcs_out=./examples_gen/ ./examples/*.proto
 protoc -I ./examples --twirpcs_out=pathPrefix=/rpc:./examples_gen/ ./examples/*.proto
+
+# For TypeScript, you can also add `protoCase=true` to generate field names in the same case as the proto file
+protoc -I ./examples --twirpts_out=pathPrefix=/rpc,protoCase=true:./examples_gen/ ./examples/*.proto
 ```
+
+## Generator Options
+
+Option | Language | Description
+-------|----------|------------
+`pathPrefix` | TypeScript, JavaScript, C# | The path prefix to use for the Twirp service. This is useful if you use `twirp.WithServerPathPrefix()`.
+`protoCase` | TypeScript | If true, field names will be generated in the same case as the proto file. If false, field names will be generated in camelCase. This is useful if you DO NOT use `twirp.WithServerJSONCamelCaseNames()`.
+
+## TypeScript
+
+This is an extremely simple generator for TypeScript, which communicates with JSON only. Assumptions:
+
+* You are OK with the `fetch()` browser API
+  * That is, you don't need to worry about older browsers that may not support it
+* Uses camelCase fields by default (ie, you have the option `twirp.WithServerJSONCamelCaseNames(true)` set)
+  * If you don't want fields changed to camelCase, use `protoCase=true` in your `twirpts_out` options, eg if you want to use the same case as the proto file  and to also generate with the path prefix `/rpc`:
+    * `--twirpts_out=pathPrefix=/rpc,protoCase=true:./`
+
+## C#
+
+This is an extremely simple generator for C#. This client makes requests using binary serialized Protobuf.
+
+On error, the generated functions will throw a `GeneratedAPI.Exception`.
 
 ## JavaScript
 
-This is an extremely simple generator for JavaScript, which communicates with JSON only. It makes a few assumptions to keep things simple:
+Another extremely simple generator, but for JavaScript, which communicates with JSON only. It makes a few assumptions to keep things simple:
 
 * Your Twirp server has the option `twirp.WithServerJSONCamelCaseNames(true)` set
   * This client uses proto3 JSON serialization instead of the snake-case default in Twirp
 * You are OK with the `fetch()` browser API
   * That is, you don't need to worry about older browsers that may not support it
-
-## C#
-
-Another extremely simple generator, but for C#. This client makes requests using binary serialized Protobuf.
-
-On error, the generated functions will throw a `GeneratedAPI.Exception`.
 
 ## Unity Example
 
---
+++ cmd/protoc-gen-twirpts/main.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"flag"
+	"strings"
+	"text/template"
+
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	pluginpb "google.golang.org/protobuf/types/pluginpb"
+)
+
+const (
+	outFileName = "generated.ts"
+)
+
+//go:embed template.tmpl
+var fileTemplate string
+
+func main() {
+	// Set up our flags. The only one we care about for now is the server path prefix.
+	var flags flag.FlagSet
+	prefix := flags.String("pathPrefix", "/twirp", "the server path prefix to use, if modified from the Twirp default")
+	useProtoCase := flags.Bool("protoCase", false, "override the default of using camelCase for field names, and use whatever was in the proto file instead")
+
+	// No special options for this generator
+	opts := protogen.Options{ParamFunc: flags.Set}
+	opts.Run(func(plugin *protogen.Plugin) error {
+		plugin.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		out := plugin.NewGeneratedFile(outFileName, "")
+
+		in := jsData{
+			Files:      plugin.Files,
+			PathPrefix: *prefix,
+			GenTypes:   make(map[string]string),
+			GenEnums:   make(map[string]string),
+			ProtoCase:  *useProtoCase,
+		}
+
+		tsTemplate, err := template.New("file").
+			Funcs(template.FuncMap{
+				"JSName":  JSName,
+				"GetType": in.GetType,
+				"GenMsg":  in.GenerateMessage,
+			}).
+			Parse(fileTemplate)
+		if err != nil {
+			return err
+		}
+
+		return tsTemplate.Execute(out, in)
+	})
+}
+
+type jsData struct {
+	Files      []*protogen.File
+	PathPrefix string
+	// Maps the type name to the TypeScript type itself
+	GenTypes  map[string]string
+	GenEnums  map[string]string
+	ProtoCase bool
+}
+
+func (j *jsData) GetType(desc protoreflect.FieldDescriptor) string {
+	switch {
+	case desc.IsMap():
+		return j.generateMap(desc)
+	default:
+		switch desc.Kind() {
+		case protoreflect.BoolKind:
+			return "boolean"
+		case protoreflect.EnumKind:
+			return j.generateEnum(desc.Enum())
+		case protoreflect.Int32Kind:
+			return "number"
+		case protoreflect.Sint32Kind:
+			return "number"
+		case protoreflect.Uint32Kind:
+			return "number"
+		case protoreflect.Sfixed32Kind:
+			return "number"
+		case protoreflect.Fixed32Kind:
+			return "number"
+		case protoreflect.FloatKind:
+			return "number"
+		case protoreflect.Int64Kind:
+			return "bigint"
+		case protoreflect.Sint64Kind:
+			return "bigint"
+		case protoreflect.Uint64Kind:
+			return "bigint"
+		case protoreflect.Sfixed64Kind:
+			return "bigint"
+		case protoreflect.Fixed64Kind:
+			return "bigint"
+		case protoreflect.DoubleKind:
+			return "number"
+		case protoreflect.StringKind:
+			return "string"
+		case protoreflect.BytesKind:
+			// NOT using Uint8Array here, as these end up getting encoded/decoded as base64 strings
+			return "string"
+		case protoreflect.MessageKind:
+			switch desc.Message().FullName() {
+			case "google.protobuf.Timestamp":
+				return "Date"
+			case "google.protobuf.Struct":
+				return "StringMap<any>"
+			case "google.protobuf.Value":
+				return "any"
+			case "google.protobuf.ListValue":
+				return "any[]"
+			case "google.protobuf.NullValue":
+				return "null"
+			case "google.protobuf.Empty":
+				return "{}"
+			case "google.protobuf.Any":
+				return "any"
+			default:
+				return j.GenerateMessage(desc.Message())
+			}
+		case protoreflect.GroupKind: // Not supported - explicitly a deprecated Protobuf feature
+			return "any"
+		default:
+			return "any"
+		}
+	}
+}
+
+// JSName exists as a way to get our camelCase method name.
+func JSName(m *protogen.Method) string {
+	if m.GoName == "" {
+		return ""
+	}
+	return strings.ToLower(m.GoName[:1]) + m.GoName[1:]
+}
+
+func (j *jsData) GenerateMessage(msg protoreflect.MessageDescriptor) string {
+	buf := bytes.Buffer{}
+	fields := msg.Fields()
+	for i := 0; i < fields.Len(); i++ {
+		field := fields.Get(i)
+		buf.WriteString("\t")
+		if j.ProtoCase {
+			buf.WriteString(string(field.Name()))
+		} else {
+			buf.WriteString(field.JSONName())
+		}
+		buf.WriteString("?: ")
+		buf.WriteString(j.GetType(field))
+		if field.IsList() {
+			buf.WriteString("[]")
+		}
+		buf.WriteString(";\n")
+	}
+
+	typeDef := buf.String()
+	typeName := convertFullName(msg.FullName())
+	j.GenTypes[typeName] = typeDef
+
+	return typeName
+}
+
+func convertFullName(name protoreflect.FullName) string {
+	names := strings.Split(string(name), ".")[1:]
+	return strings.Join(names, "_")
+}
+
+func (j *jsData) generateEnum(enum protoreflect.EnumDescriptor) string {
+	// Enums can be represented as either numbers or strings, but strings are easier to read
+	buf := bytes.Buffer{}
+	for i := 0; i < enum.Values().Len(); i++ {
+		value := string(enum.Values().Get(i).Name())
+		buf.WriteString("\t")
+		buf.WriteString(value)
+		buf.WriteString(` = "`)
+		buf.WriteString(value)
+		buf.WriteString("\",\n")
+	}
+
+	typeDef := buf.String()
+	typeName := convertFullName(enum.FullName())
+	j.GenEnums[typeName] = typeDef
+
+	return typeName
+}
+
+func (j *jsData) generateMap(desc protoreflect.FieldDescriptor) string {
+	// Now we need to parse the map type - the key is always one of the primitive types,
+	// but the value can be a message or a primitive type.
+	buf := bytes.Buffer{}
+	switch j.GetType(desc.MapKey()) {
+	case "number":
+		buf.WriteString("NumberMap<")
+	default:
+		buf.WriteString("StringMap<")
+	}
+	buf.WriteString(j.GetType(desc.MapValue()))
+	buf.WriteString(">")
+
+	return buf.String()
+}
